835fe0fa4d588c8de57e1066d3c676ad49ac08b1
dahlias-x <dahliaberries@gmail.com>
2021-05-17 23:21:15 +0200

    schreibfehler

--- Registiration_Form.js
+++ Registiration_Form.js
@@ -6,24 +6,22 @@ const errorElement = document.getElementById('error')
 form.addEventListener('submit',(e) => {
     let messages = []
 
-    if (name.value === '' || name.value == null ){
+    if (name.value === '' || name.value == null){
         messages.push('Name is required')
     }
 
-    if (password.length <=6) {
-            messages.push('Password must be longer than 6 charachters')
+    if (password.value.length <= 6) {
+            messages.push('Password must be longer than 6 characters')
     }
-    if (password.length >= 20) {
-        messages.push('Password must be shorter than 20 charachters')
-
+    if (password.value.length >= 20) {
+        messages.push('Password must be less than 20 characters')
     }
-    if (password.value === 'password'){
+    if (password.value === 'password') {
         messages.push('Password cannot be password')
-        {
+    }
         
-    if (name.lenght > 0){
+    if (messages.length > 0) {
     e.preventDefault() 
     errorElement.innerText = messages.join(', ')
     }
-
 })
